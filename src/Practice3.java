368bfe4cf8261a43c82cdba32086bec5d911bee4
Zukhrahon <zukhra.doctor1@gmail.com>
2022-03-14 20:29:16 -0500

    I added statement on Line 9

--- src/Practice3.java
+++ src/Practice3.java
@@ -1,11 +1,13 @@
 public class Practice3 {
+    public static void main(String[] args) {
+
 
 //HELLO! hello?
     // hello fro AO
     //hello from me 2
  // I have to eat ice-cream!
-     String str= null;
+     String str= "Zukhra";
 
     String str = "I don't like ice-cream!";
 
-}
+}}
