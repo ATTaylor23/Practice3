2f4bb5c76998c8e543873350216aa485afba6be6
Andrey O <opexandrey@gmail.com>
2022-03-14 21:29:43 -0400

    ao commit 3

--- src/Practice3.java
+++ src/Practice3.java
@@ -1,11 +1,17 @@
 public class Practice3 {
 
+    public static void main(String[] args) {
+
+
 //HELLO! hello?
-    // hello fro AO
-    //hello from me 2
- // I have to eat ice-cream!
+        // hello fro AO
+        //hello from me 2
+        // I have to eat ice-cream!
      String str= null;
 
     String str = "I don't like ice-cream!";
 
+        String str = "Zuhra";
+
+    }
 }
